16b177649b4b04bef6c344aec01b2237207d14f0
Ronald200202 <ronal.cahyadi55@gmail.com>
2025-04-03 00:53:12 +0800

    update lagi desain ampas

--- static/js/result_nondiabetes.js
+++ static/js/result_nondiabetes.js
@@ -3,38 +3,33 @@ $(document).ready(function(){
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 3000,
-      prevArrow: $('.prev'),
+      autoplaySpeed: 2000,
+      prevArrow: $('.prev'),  // Pastikan elemen ada di dalam HTML
       nextArrow: $('.next'),
-      dots: true,
       responsive: [
         {
-          breakpoint: 1200,
+          breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
           }
         },
         {
-          breakpoint: 768,
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
           }
         }
       ]
-  });
-  
-  // Add animation on scroll
-  $(window).scroll(function() {
-      $('.card, .post').each(function() {
-          var position = $(this).offset().top;
-          var scroll = $(window).scrollTop();
-          var windowHeight = $(window).height();
-          
-          if (scroll > position - windowHeight + 200) {
-              $(this).addClass('visible');
-          }
-      });
   });
 });
